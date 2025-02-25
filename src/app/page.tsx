67b716eaa2d6ea894c5a45f6b949ac55eb9f3bb6
Fernando <fernandoventurini9@hotmail.com>
2025-02-25 20:05:24 +0000

    created hook for state change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 //import Image from "next/image";
 // import Menu from "@/components/Menu";
 // import Footer from "@/components/Footer";
+'use client';
 
 import User from "@/components/User";
+import { useState } from "react";
 
 const Home = () => {
+
+  const [nameUser, setNameUser] = useState('Cesar');
 
   const userName = 'Cesar';
 
   return (
     <main>      
+      <p>Nome: {nameUser}</p>
+      <button onClick={() => setNameUser('Fernando!')}>Alterar nome</button>
+
       <User name={userName}>
         <p>Este e um comentario extra fornecido como children.</p>
       </User>
