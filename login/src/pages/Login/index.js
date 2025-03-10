4d57afc259c425ce5bc4e3bd364f6c1d2068a94a
Fernando <fernandoventurini9@hotmail.com>
2025-03-10 18:27:30 +0000

    creating login form #03

--- login/src/pages/Login/index.js
+++ login/src/pages/Login/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Login = () => {
+
+    // USANDO USESTATE PARA RECEBER O QUE FOR DIGITADO NOS INPUTS:
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    // CRIANDO FUNCAO PARA RECEBER OS VALORES:
+    const valorInput = e => setUser({...user, [e.target.name]: e.target.value});
+
+    const loginSubmit = async e => {
+        e.preventDefault();
+        console.log(user.email);
+    }
+
     return (
         <>
             <h1>Login</h1>
+            <form onSubmit={loginSubmit}>
+                <label>Usuario: </label>
+                <input type='text' name='email' placeholder='Digite o e-mail...' onChange={valorInput}/>
+
+                <label>Senha: </label>
+                <input type='password' name='password' placeholder='Digite a senha...' onChange={valorInput}/>
+
+                <button type='submit'>Acessar</button>
+            </form>
         </>
     );
 }
